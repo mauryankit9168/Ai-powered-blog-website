7b47d0fa8a4ac462a7e91a56bbeebcfe9c70240e
ankit maurya <ankit14082004@email.com>
2026-06-18 00:51:20 +0530

    Added blog website components

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { motion } from 'motion/react'
-import { blogCategories } from '../assets/assets'
+import { blog_data, blogCategories } from '../assets/assets'
+import BlogCard from './BlogCard'
 
 const BlogList = () => {
 
@@ -29,6 +30,10 @@ const BlogList = () => {
             </button>
           </div>
         ))}
+      </div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40'>
+        {blog_data.filter((blog) => menu === "All" ? true : blog.category === menu).
+          map((blog) => <BlogCard key={blog._id} blog={blog}/>)}
       </div>
     </div>
   )

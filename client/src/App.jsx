bb43b0732e552f7e8415aa58a61554ee197330f2
ankit maurya <ankit14082004@gmail.com>
2026-06-28 00:31:03 +0530

    updated Comment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,13 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
+import Layout from './pages/admin/Layout'
+import Dashboard from './pages/admin/Dashboard'
+import Comments from './pages/admin/Comments'
+import AddBlog from './pages/admin/AddBlog'
+import ListBlog from './pages/admin/ListBlog'
+import Login from './components/admin/Login'
+import 'quill/dist/quill.snow.css';
 
 const App = () => {
   return (
@@ -10,6 +17,12 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/blog/:id' element={<Blog />} />
+        <Route path='/admin' element={true?<Layout />: <Login/>}>
+          <Route index element={<Dashboard />} />
+          <Route path='comments' element={<Comments />} />
+          <Route path='addBlog' element={<AddBlog />} />
+          <Route path='listBlog' element={<ListBlog />} />
+        </Route>
       </Routes>
     </div>
   )
